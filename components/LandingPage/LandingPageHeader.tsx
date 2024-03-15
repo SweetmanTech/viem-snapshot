4d59231643049caae712e0901be4c12ca24737a0
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-15 10:58:19 -0300

    initial commit

--- components/LandingPage/LandingPageHeader.tsx
+++ components/LandingPage/LandingPageHeader.tsx
@@ -1,4 +1,4 @@
-const LandingPageHeader = ({ isServer }: any) => (
+const LandingPageHeader = () => (
   <div
     style={{ display: 'flex' }}
     className="space-y-3 flex flex-col"
@@ -8,13 +8,13 @@ const LandingPageHeader = ({ isServer }: any) => (
       className="text-4xl font-bold tracking-tighter sm:text-5xl"
       tw="text-7xl font-black pt-[100px]"
     >
-      Know Your Community.
+      viem snapshot.
     </p>
     <p
       className="max-w-[600px] text-md md:text-xl font-bold"
       tw="max-w-[600px] text-4xl font-bold mt-[-25]"
     >
-      {!isServer && `Log in to`} view your most loyal collectors
+      showing founders pass snapshot for cre8ors
     </p>
   </div>
 );
