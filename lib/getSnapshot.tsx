cecd486776c3d8ee1a11546aafa47e609462a08b
Sweets Sweetman <sweetmantech@gmail.com>
2024-03-15 12:16:18 -0300

    I refactor constract name to be passports.

--- lib/getSnapshot.tsx
+++ lib/getSnapshot.tsx
@@ -2,9 +2,9 @@ import { erc721Abi } from 'viem';
 import { ethPublicClient } from './publicClient';
 
 const getSnapshot = async () => {
-  const cre8orsAddress = '0xd9635b70724b9f618a7bb37c7be182117b1f0dc1';
+  const cre8orsPassportAddress = '0xd9635b70724b9f618a7bb37c7be182117b1f0dc1';
   const wagmiContract = {
-    address: cre8orsAddress,
+    address: cre8orsPassportAddress,
     abi: erc721Abi,
   } as const;
   const contracts = Array.from({ length: 88 }, (_, index) => ({
